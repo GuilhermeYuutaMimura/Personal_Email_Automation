3bf6622c3c2e9661c2cc3e88445e066850deb49c
Guilherme Yuuta Mimura <guilhermeymimura@gmail.com>
2026-01-21 18:09:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ works_done = 0
 
 file = open('classes.txt','r')
 content_data = file.read()
+content = content_data
 file.close()
 
-for i in range(len(content_data)):
-    if ('due') in content_data[i]:
-        works_due +=1
-    if('done') in content_data[i]:
-        works_done +=1
-    else:
-        pass
+for line in content_data:
+    words = line.strip().split()
+    for i in range:
+        if (i == 'due'):
+            works_due +=1
+        if (i == 'done'):
+            works_done +=1
+        else:
+            pass
 
-print(content_data)
-print(works_due)
-print(works_done)
+print(content)
+print(words)
+#print(works_due)
+#print(works_done)#
