fa138208ba4bbdc36eaeb43d4f11a42a7d742bd2
Guilherme Yuuta Mimura <guilhermeymimura@gmail.com>
2026-01-21 16:15:17 -0300

    Add classes.txt and update main.py to read file
    Added a new classes.txt file containing class and assignment information. Updated main.py to open and read the contents of classes.txt, preparing for further processing of the file's lines.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from datetime import date
-from workalendar.america import BrazilSaoPauloState
+#from datetime import date
+#from workalendar.america import BrazilSaoPauloState
+styled_file = []
+
+file = open('classes.txt','r')
+read_file = file.readlines()
+
+for line in _file:
+    if (line[-1] == '\n'):
+        styled_file
+print(read_file)
